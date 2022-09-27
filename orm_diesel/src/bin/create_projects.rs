51ce93e02f9ea40054cf35d2e0a34e629aca1cfb
GhosTHaise <Ghostrex2@gmail.com>
2022-09-27 22:21:09 +0300

    struturing orm library

--- orm_diesel/src/bin/create_projects.rs
+++ orm_diesel/src/bin/create_projects.rs
@@ -7,13 +7,12 @@ use self::orm_diesel::*;
 fn main() {
 
     //use self::schema::projects::dsl::*;
-
-    let connection = establish_connection();
+    let orm : OrmDiesel = orm_diesel::OrmDiesel::new("project".to_string());
     let title = "API withb rust";
     let url = "https://GhosTHaise@github.com";
     let created_at = "23/09/2022:16:17:01";
     /* let project = create_post(&connection,title,url,created_at);
     println!("Saved project : {}, with id : {} successfully",project.title,project.id); */
-    let data = new_project(&title,&url,&created_at);
-    create_post(&connection,&data);
+    let data = orm.new_project(&title,&url,&created_at);
+    orm.create_post(&data);
 }
--- orm_diesel/src/lib.rs
+++ orm_diesel/src/lib.rs
@@ -10,8 +10,10 @@ pub mod schema;
 use diesel::{prelude::*, insert_into};
 use dotenv::dotenv;
 use std::env;
-
-
+use schema::projects::dsl::*;
+pub struct OrmDiesel{
+    pub connection : MysqlConnection
+}
 pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
 
@@ -19,22 +21,32 @@ pub fn establish_connection() -> MysqlConnection {
     MysqlConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting"))
 }
 
-use schema::projects::dsl::*;
-pub fn create_post(conn : &MysqlConnection,data : &model::NewProject) -> () {
-    //let json = r#"{"title":"Test Insertion","url":"sdfsdfsdf","created_at":"24 septembre"}"#;
-    //let project_json = serde_json::from_str::<model::NewProject>(json).unwrap();
-    /* let new_data = model::NewProject{
-        title : "Bonjour ...",
-        url : "asdasdas@gamilad.com",
-        created_at : "24 septembre 2022"
-    };
-     */
-    insert_into(projects).values(data).execute(conn);
-}
-pub fn new_project<'a>(_title : &'a str,_url : &'a str,_created_at : &'a str) -> model::NewProject<'a> {
-    model::NewProject{
-        title : _title,
-        url : _url,
-        created_at : _created_at
+impl OrmDiesel{
+    pub fn new(_model : String) -> OrmDiesel{
+        OrmDiesel{
+            connection : establish_connection()
+        }
     }
+      
+    pub fn create_post(&self,data : &model::NewProject) -> () {
+        //let json = r#"{"title":"Test Insertion","url":"sdfsdfsdf","created_at":"24 septembre"}"#;
+        //let project_json = serde_json::from_str::<model::NewProject>(json).unwrap();
+        /* let new_data = model::NewProject{
+            title : "Bonjour ...",
+            url : "asdasdas@gamilad.com",
+            created_at : "24 septembre 2022"
+        };
+         */
+        insert_into(projects).values(data).execute(&self.connection);
+    }
+    pub fn new_project<'a>(&self,_title : &'a str,_url : &'a str,_created_at : &'a str) -> model::NewProject<'a> {
+        model::NewProject{
+            title : _title,
+            url : _url,
+            created_at : _created_at
+        }
+    }
+
 }
+
+
